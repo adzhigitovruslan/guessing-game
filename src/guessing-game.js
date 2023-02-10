f2d962177b502d3e652b436f9033a50c7072fda3
adzhigitovruslan <adzhigitovruslan@gmail.com>
2023-02-10 20:47:45 +0800

    feat: add js function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min,
+        this.right = max
     }
 
     guess() {
-
+        this.mid = Math.round((this.right - this.left)/2) + this.left;
+        return this.mid
     }
 
     lower() {
-
+        this.right = this.mid;
+        return this.right
     }
 
     greater() {
-
+        this.left = this.mid;
+        return this.left
     }
 }
 
